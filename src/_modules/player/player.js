d02802491c29ef57a6ec262083876e3c6da42b0e
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2019-12-02 12:12:54 +1100

    - Changed CSS designs to fit for kindle

--- src/_modules/player/player.js
+++ src/_modules/player/player.js
@@ -2,6 +2,23 @@
 
 export default class Player {
   constructor() {
+    $('.planeswalker__logo').css({
+      top: $(window).height() / 2 - $('.planeswalker__logo').height() / 2
+    });
+
+    $('.player, .player__controls, .player__control').css({
+      height: $(window).height() / 2
+    });
+
+    $('.player__life span').css({
+      left: $(window).width() / 2 - $('.player__life span').width() / 2,
+      top: $('.player').height() / 2 - $('.player__life span').height() / 2
+    });
+
+    $('.player__controls span').css({
+      top: $('.player').height() / 2 - $('.player__controls span').height() / 2
+    });
+
     $('.-js-life-up').on('click', function (e) {
       e.preventDefault();
 
